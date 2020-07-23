2bfe6eecd20baa06a1d22ed2676fb674d8dfcd24
nshah-sfoundation <36900649+nshah-sfoundation@users.noreply.github.com>
2020-07-23 16:29:50 -0400

    Update geocode.rb

--- bin/geocode.rb
+++ bin/geocode.rb
@@ -1,16 +1,7 @@
-require 'rubygems'
-require 'csv'
 require 'geocoder/us'
 require 'json'
 
 db = Geocoder::US::Database.new("/opt/geocoder.db")
 
 out = db.geocode(ARGV[0])
-
-# write to json file for processing with R
-File.open(ARGV[1],"w") do |f|
-    f.write(out.to_json)
-end 
-
-
-
+puts out.to_json
